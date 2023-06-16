f2641f6397233175e64471e6b0f12dde2c6def12
motoki_tamagawa <motoki.tamagawa@person-link.co.jp>
2023-06-16 17:48:03 +0900

    ref/1

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
+  <div class="footer-wrapper">
     フッター
     <div>
       <router-link :to="'policy'">プライバシーポリシー</router-link>
     </div>
   </div>
 </template>
+
+
+<style>
+
+.footer-wrapper {
+  height: 140px;
+  background-color:#e3e2e0;
+  margin: auto;
+  text-align: center;
+}
+
+</style>
--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -2,22 +2,28 @@
 
   <div class="header-container">
 
-    <di class="header-contents">
+    <div class="header-contents">
       <div class="header-left">
-        <div>行政書士 寺町久徳事務所</div>
+        <router-link :to="'/'" style="text-decoration: none;  color: black;">行政書士 寺町久徳事務所</router-link >
+          <div class="heade-en">TERAMACHI HISANORI ADMINISTRATIVE SCRIVENER OFFICE</div>
       </div>
 
       <div class="header-right">
-        <div class="header-right-content">STATEMENT</div>
-        <div class="header-right-content">事業内容</div>
-        <div class="header-right-content">事務所概要</div>
         <div class="header-right-content">
-          <router-link :to="'/contact'">お問い合わせ</router-link>
+          <div class="en">FIELD</div>
+          <div class="jp">事業内容</div>
+        </div>
+        <div class="header-right-content">
+          <div class="en">CORPORATE</div>
+          <div class="jp">事務所概要</div>
+        </div>
+        <div class="header-right-content">
+          <div class="en">CONTACT</div>
+          <div class="jp"><router-link :to="'/contact'" style="text-decoration: none;  color: black;">お問い合わせ</router-link></div>
+
         </div>
       </div>
-    </di>
-
-
+    </div>
 
   </div>
 </template>
@@ -26,16 +32,21 @@
 
 .header-container {
   padding: 20px 20px;
-  background-color: rgb(0, 183, 79);
-  color: #fff;
+  background-color:#ffffff;
+  color:  rgb(0, 0, 0);
   font-weight: bold;
+  height: 50px;
+  /* position: fixed; */
+  margin: auto;
+  z-index: 999;
+  width: 1000px;
 }
 
 .header-contents {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  max-width: 800px;
+  max-width: 1000px;
   margin: auto;
 }
 
@@ -43,16 +54,32 @@
 .header-left {
   text-align: left;
   font-size: 22px;
+  font-family:serif;
+  text-align: center;
+}
+
+.heade-en {
+ font-size: 8px;
 }
 
 .header-right {
   display: flex;
   justify-content: center;
   align-items: space-between;
+  font-family:serif;
 }
 
 .header-right-content {
   padding: 0px 20px;
+  text-align: center;
+}
+
+.en {
+  font-size: 16px;
+}
+
+.jp {
+  font-size: 12px;
 }
 
 </style>
